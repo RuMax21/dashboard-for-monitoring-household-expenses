65bcc99c5b74409964f98e87879bd9c04c68cead
RuMax21 <cherednikov.maxim@mail.ru>
2025-12-07 03:04:37 +0300

    add Docker config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { BudgetsModule } from './budgets/budgets.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CategoriesModule, TransactionsModule, BudgetsModule, NotificationsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
